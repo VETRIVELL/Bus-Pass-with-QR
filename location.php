7a6c0a01583eab27b3d731bad088bce3a8aea745
VETRIVELL <142883877+VETRIVELL@users.noreply.github.com>
2023-08-22 14:38:22 +0530

    Add files via upload

---
+++ location.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>MTC ONLINE TICKET BOOKING</title>
+<link rel="stylesheet" type="text/css" href="xres/css/style.css" />
+<link rel="icon" type="image/png" href="xres/images/favicon.png" />
+<link type="text/css" href="css/styles.css" rel="stylesheet" media="all" />
+</head>
+
+<body>
+<div id="wrapper">
+	<div id="header">
+    <h1><a href="index.php"></a></h1>
+        <ul id="mainnav">
+			<li><a href="index.php">Home</a></li>
+            <li><a href="gallery.php">Gallery</a></li>
+            <li><a href="history.php">History</a></li>
+            <li><a href="routes.php">Routes</a></li>
+            <li class="current"><a href="location.php">location</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+    	</ul>
+	</div>
+    <div id="content">
+    	<div id="gallerycontainer">
+			<div class="portfolio-area" style="margin:0 auto; padding:140px 20px 20px 20px; width:820px;">	
+				<iframe width="100%" height="390" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://goo.gl/maps/JgmysKYopR24YUAPA"></iframe><br /><small><a href="https://goo.gl/maps/JgmysKYopR24YUAPA" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+				<div class="column-clear"></div>
+            </div>
+			<div class="clearfix"></div>
+        </div>
+    </div>
+    
+
+
+
+<div id="footer">
+	<h4>9345368909 &bull; <a href="contact-us.php">KOYAMBEDU BUS STAND,CHENNAI  </a></h4>
+	<p>Hours of Operation&nbsp;&nbsp;&bull;&nbsp;&nbsp;Mon - Sun: 10:00 am - 12:00 am</p>
+	<a href="index.php"></a>
+	<p>&copy;  <br /></p>
+</div>
+
+</div>
+</body>
+</html>
